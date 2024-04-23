4a2002a7aecf3051b3a48c70f833d8d7993520e3
radwane212 <redzmourine671@gmail.com>
2024-04-23 22:43:19 +0200

    commiting file

---
+++ 0x13-javascript_objects_scopes_closures/102-concat.js
@@ -0,0 +1,29 @@
+const fs = require('fs');
+const args = process.argv.slice(2);
+if (args.length === 3) {
+  const fileA = args[0];
+  const fileB = args[1];
+  const fileC = args[2];
+  let concate = '';
+  fs.readFile(fileA, 'utf-8', (err, data) => {
+    if (err) {
+      console.error('Error Reading from ' + fileA + ' : ' + err);
+      return;
+    }
+    concate += data;
+    fs.readFile(fileB, 'utf-8', (err, data) => {
+      if (err) {
+        console.error('Error Reading from ' + fileB + ' : ' + err);
+        return;
+      }
+      concate += data;
+      fs.writeFile(fileC, concate, 'utf-8', (err) => {
+        if (err) {
+          console.error('Erro writing to ' + fileC + ' : ' + err);
+        }
+      });
+    });
+  });
+} else {
+  console.error('REQUIRED : files to concatenate and output file.');
+}
