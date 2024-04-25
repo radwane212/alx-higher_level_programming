cd21b47cb24e629e4bb6bc532e4b83dcf47a3b3b
radwane212 <redzmourine671@gmail.com>
2024-04-25 22:04:49 +0200

    lists all State objects that contain the letter a from the database hbtn_0e_6_usa

---
+++ 0x0F-python-object_relational_mapping/9-model_state_filter_a.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""import libraries"""
+from model_state import Base, State
+from sqlalchemy import create_engine, text
+from sys import argv
+
+
+def main():
+    engine = create_engine('mysql://{}:{}@localhost/{}'.format(
+        argv[1], argv[2], argv[3]))
+    conn = engine.connect()
+    out = conn.execute(text("SELECT * FROM states"))
+    for i in out:
+        if 'a' in i[1]:
+            print("{}: {}".format(i[0], i[1]))
+
+
+if __name__ == "__main__":
+    main()
