42ebee932fce5c40e5f80b78939549566c19febf
radwane212 <redzmourine671@gmail.com>
2024-05-01 19:57:48 +0200

    evrtying updat

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -1,30 +1,21 @@
 #!/usr/bin/python3
-"""
-List all cases from database hbtn_0e_0_usa
-"""
+"""lists all states from the database hbtn_0e_0_usa"""
+
 import MySQLdb
 import sys
 
-
-def main():
-    args = sys.argv[1:]
-    username = args[0]
-    password = args[1]
-    database = args[2]
-    connection = MySQLdb.connect(
-        user=username,
-        password=password,
-        db=database,
-        host="localhost",
-        port=3306
-    )
-    cursor = connection.cursor()
-    cursor.execute("SELECT * FROM states ORDER BY id ASC")
-    for i in cursor.fetchall():
-        print(i)
-    cursor.close()
-    connection.close()
-
-
 if __name__ == "__main__":
-    main()
+    db = MySQLdb.connect(
+        host="localhost",
+        port=3306,
+        user=sys.argv[1],
+        passwd=sys.argv[2],
+        db=sys.argv[3],
+    )
+    cur = db.cursor()
+    cur.execute("SELECT * FROM states ORDER BY id ASC")
+    rows = cur.fetchall()
+    for row in rows:
+        print(row)
+    cur.close()
+    db.close()
