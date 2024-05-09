9051abb929a6f7e4b9ede3594c53bef4ecec6f0b
radwane212 <redzmourine671@gmail.com>
2024-05-09 11:15:41 -0700

    running on port 5000

--- 0x10-python-network_0/1-body.sh
+++ 0x10-python-network_0/1-body.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-# Get the response body for a given URL for 200 status code responses.
-curl -sL "$1"
+# A Bash script that takes a URL, sends a GET request to the URL, and displays the response body.
+curl -sL GET "$1"
